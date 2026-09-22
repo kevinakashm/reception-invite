c283631fc20858f495057dd8a644185d47da6ee6
kevinakashm <kevinakash.m@gmail.com>
2026-09-22 19:21:01 +0530

    Added metadata

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,10 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Reception Invitation</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+          Blessed by family, surrounded by love, and ready for forever — come celebrate with us! ✨
+        </meta>
+        <title>Arvinth weds Mohanapriya</title>
       </Head>
       <Component {...pageProps} />
     </>
