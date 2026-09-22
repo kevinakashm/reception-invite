44f9b58c2dfd5b82d6d8b744cff06ad02431b0af
kevinakashm <kevinakash.m@gmail.com>
2026-09-22 19:30:25 +0530

    Meta data fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-          Blessed by family, surrounded by love, and ready for forever — come celebrate with us! ✨
-        </meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Blessed by family, surrounded by love, and ready for forever — come celebrate with us! ✨"
+        />
         <title>Arvinth weds Mohanapriya</title>
       </Head>
       <Component {...pageProps} />
